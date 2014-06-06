b602f39b43db41b123458c5a7813598fa650664b
liu liu <leoliu@u.northwestern.edu>
2014-06-06 15:01:26 -0700

    classification functions

---
+++ user_classify.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 06 14:11:55 2014
+
+@author: liu
+"""
+
+from subroutines import *
+catSku = {}
+#catsku is the category sku dictionary. For each category, there are multiple skus. This is a dictionary of key->list
+userSku = {}
+#for each user, he/she has different sku purchased. This is a dictionary of key -> list
+userCat = {}
+#for each user, he/she must have bought items from different catagories. 
+#his is a dictionary of key->dictionary
+catSku = readFile('catSku.dat')
+userSku = readFile('userSku.dat')
+userCat = readFile('userCat.dat')
+
+#now all we left to do is to classfy the users, and also to classify the items. 
+#The items are all stored in catSku. I assume that the item type would be good enough to classfy the items? I dont know. 
+#We will see. 
